e1d933f53ef72d9896405209bb9645052bd0a218
Andrei <kulakov1@tut.by>
2023-02-28 22:10:30 +0300

    second commit

--- django_app/configs/urls.py
+++ django_app/configs/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from my_app.views import page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', page),
 ]
--- gateway_app/server.py
+++ gateway_app/server.py
@@ -2,12 +2,18 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 PORT = 8050
 
+
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('content-type', 'text/html')
         self.end_headers()
         self.wfile.write("hello !".encode())
+        if self.path == '/weather':
+            print(1)
+        elif self.path == '/bank':
+            print(1)
+
 
 serv = HTTPServer(("localhost",PORT), Handler)
 serv.serve_forever()
